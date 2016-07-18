880a1ce4d5a995163e1998902a417c1ce11dca89
Brendon Body <bbody.project@gmail.com>
2016-07-18 21:06:55 +1000

    Move resources to remote

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -9,12 +9,12 @@
  */
 angular.module('portfolioApp')
   .constant("endPoints", {
-    // "portfolio_technologies": "https://s3-ap-southeast-2.amazonaws.com/bbody/Details/portfolio_technologies.json",
-    // "portfolio": "https://s3-ap-southeast-2.amazonaws.com/bbody/Details/portfolio.json",
-    // "technologies":"https://s3-ap-southeast-2.amazonaws.com/bbody/Details/technologies.json",
-    // "information": "https://s3-ap-southeast-2.amazonaws.com/bbody/Details/details.json",
-    "portfolio_technologies": "responses/portfolio_technologies.json",
-    "portfolio": "responses/portfolio.json",
-    "technologies":"responses/technologies.json",
-    "information": "responses/details.json"
+    "portfolio_technologies": "https://s3-ap-southeast-2.amazonaws.com/bbody/Details/portfolio_technologies.json",
+    "portfolio": "https://s3-ap-southeast-2.amazonaws.com/bbody/Details/portfolio.json",
+    "technologies":"https://s3-ap-southeast-2.amazonaws.com/bbody/Details/technologies.json",
+    "information": "https://s3-ap-southeast-2.amazonaws.com/bbody/Details/details.json",
+    // "portfolio_technologies": "responses/portfolio_technologies.json",
+    // "portfolio": "responses/portfolio.json",
+    // "technologies":"responses/technologies.json",
+    // "information": "responses/details.json"
   });
