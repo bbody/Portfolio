dc4cbf04c2e5eaacd5a3421c19a4946a567ddf9e
Brendon Body <bbody.project@gmail.com>
2016-07-23 14:27:50 +1000

    Fix up commits

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -12,7 +12,7 @@ angular.module('portfolioApp')
     "portfolio_technologies": "https://s3-ap-southeast-2.amazonaws.com/bbody/Details/portfolio_technologies.json",
     "portfolio": "https://s3-ap-southeast-2.amazonaws.com/bbody/Details/portfolio.json",
     "technologies":"https://s3-ap-southeast-2.amazonaws.com/bbody/Details/technologies.json",
-    "information": "https://s3-ap-southeast-2.amazonaws.com/bbody/Details/details.json",
+    "information": "https://s3-ap-southeast-2.amazonaws.com/bbody/Details/details.json"
     // "portfolio_technologies": "responses/portfolio_technologies.json",
     // "portfolio": "responses/portfolio.json",
     // "technologies":"responses/technologies.json",
